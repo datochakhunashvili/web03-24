910e62d2a788f5fa4783796e66c218532fdf5be2
datochakhunashvili <130147418+datochakhunashvili@users.noreply.github.com>
2023-05-31 16:03:55 +0400

    add. 1. js true,false

---
+++ script.js
@@ -0,0 +1,9 @@
+const getEven = (number) => {
+    if (number %2 === 0 * 3) {
+        return "true";
+    } else{
+        return "false";
+    };
+};
+console. log (getEven(6));
+console. log (getEven(7));
